6259c43c529ae426b9bbfcc99d643a92b284e7b2
Junior Ibarra <Ibarra_0811@yahoo.com>
2021-02-21 13:17:59 -0700

    committed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.post('/api/movies', ctrl.addMovie)
 app.put('/api/movies/:id', ctrl.editMovie)
 app.delete('/api/movies/:id', ctrl.deleteMovie)
 
-const port = 4000
+const port =  4000
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
